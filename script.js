c1e094db68d47ef5b2592af7389b8bfb45c79bba
YuderLin <91525008+YuderLin@users.noreply.github.com>
2021-12-12 12:08:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,192 @@
+//news
+$('.news').mouseover(function(event){
+	$('.news').css('color', '#F9C2C2')
+});
+$('.news').click(function(event){
+	$('.news').css('color', '#440707')
+});
+$('.news').mouseleave(function(event){
+	$('.news').css('color', '#FFFFFF')
+});
+
+//introduction
+$('.introduction').mouseover(function(event){
+	$('.introduction').css('color', '#F9C2C2')
+});
+$('.introduction').click(function(event){
+	$('.introduction').css('color', '#440707')
+});
+$('.introduction').mouseleave(function(event){
+	$('.introduction').css('color', '#FFFFFF')
+});
+
+//character
+$('.character').mouseover(function(event){
+	$('.character').css('color', '#F9C2C2')
+});
+$('.character').click(function(event){
+	$('.character').css('color', '#440707')
+});
+$('.character').mouseleave(function(event){
+	$('.character').css('color', '#FFFFFF')
+});
+
+//staff
+$('.staff').mouseover(function(event){
+	$('.staff').css('color', '#F9C2C2')
+});
+$('.staff').click(function(event){
+	$('.staff').css('color', '#440707')
+});
+$('.staff').mouseleave(function(event){
+	$('.staff').css('color', '#FFFFFF')
+});
+
+//music
+$('.music').mouseover(function(event){
+	$('.music').css('color', '#F9C2C2')
+});
+$('.music').click(function(event){
+	$('.music').css('color', '#440707')
+});
+$('.music').mouseleave(function(event){
+	$('.music').css('color', '#FFFFFF')
+});
+
+//book
+$('.book').mouseover(function(event){
+	$('.book').css('color', '#F9C2C2')
+});
+$('.book').click(function(event){
+	$('.book').css('color', '#440707')
+});
+$('.book').mouseleave(function(event){
+	$('.book').css('color', '#FFFFFF')
+});
+
+//+1
+$('.addmore').mouseover(function(event){
+	$('.addmore').css('color', '#F9C2C2')
+});
+$('.addmore').click(function(event){
+	$('.addmore').css('color', '#440707')
+});
+$('.addmore').mouseleave(function(event){
+	$('.addmore').css('color', '#FFFFFF')
+});
+
+//+2
+$('.addmore2').mouseover(function(event){
+	$('.addmore2').css('color', '#F9C2C2')
+});
+$('.addmore2').click(function(event){
+	$('.addmore2').css('color', '#440707')
+});
+$('.addmore2').mouseleave(function(event){
+	$('.addmore2').css('color', '#FFFFFF')
+});
+
+//kurena
+$('.Kurena').click(function(event) {
+	$('.Kurena').css('background-image', 'url(img/Kurena-active.png)');
+});
+$('.Kurena').mouseover(function(event) {
+	  $('.Kurena').css('background-image', 'url(img/Kurena-hover.png)');
+  });
+
+$('.Kurena').mouseleave(function(ebent){
+	$('.Kurena').css('background-image','url(img/Kurena_1.png)')
+});
+
+//anju
+$('.Anju').click(function(event) {
+	$('.Anju').css('background-image', 'url(img/Anju-active.png)');
+});
+$('.Anju').mouseover(function(event) {
+	  $('.Anju').css('background-image', 'url(img/Anju-hover.png)');
+  });
+
+$('.Anju').mouseleave(function(ebent){
+	$('.Anju').css('background-image','url(img/Anju_1.png)')
+});
+
+//Lena
+$('.Lena').click(function(event) {
+	$('.Lena').css('background-image', 'url(img/Lena-active.png)');
+});
+$('.Lena').mouseover(function(event) {
+	  $('.Lena').css('background-image', 'url(img/Lena-hover.png)');
+  });
+
+$('.Lena').mouseleave(function(ebent){
+	$('.Lena').css('background-image','url(img/Lena_1.png)')
+});
+
+//Shin
+$('.Shin').click(function(event) {
+	$('.Shin').css('background-image', 'url(img/Shin-active.png)');
+});
+$('.Shin').mouseover(function(event) {
+	  $('.Shin').css('background-image', 'url(img/Shin-hover.png)');
+  });
+
+$('.Shin').mouseleave(function(ebent){
+	$('.Shin').css('background-image','url(img/Shin_1.png)')
+});
+
+//Raiden
+$('.Raiden').click(function(event) {
+	$('.Raiden').css('background-image', 'url(img/Raiden-active.png)');
+});
+$('.Raiden').mouseover(function(event) {
+	  $('.Raiden').css('background-image', 'url(img/Raiden-hover.png)');
+  });
+
+$('.Raiden').mouseleave(function(ebent){
+	$('.Raiden').css('background-image','url(img/Raiden_1.png)')
+});
+
+//Theo
+$('.Theo').click(function(event) {
+	$('.Theo').css('background-image', 'url(img/Theo-active.png)');
+});
+$('.Theo').mouseover(function(event) {
+	  $('.Theo').css('background-image', 'url(img/Theo-hover.png)');
+  });
+
+$('.Theo').mouseleave(function(ebent){
+	$('.Theo').css('background-image','url(img/Theo_1.png)')
+});
+
+//reference
+$('.reference').mouseover(function(event){
+	$('.reference').css('color', '#F9C2C2')
+});
+$('.reference').click(function(event){
+	$('.reference').css('color', '#440707')
+});
+$('.reference').mouseleave(function(event){
+	$('.reference').css('color', '#FFFFFF')
+});
+
+//copyright
+$('.copyright').mouseover(function(event){
+	$('.copyright').css('color', '#F9C2C2')
+});
+$('.copyright').click(function(event){
+	$('.copyright').css('color', '#440707')
+});
+$('.copyright').mouseleave(function(event){
+	$('.copyright').css('color', '#FFFFFF')
+});
+
+//privacy
+$('.privacy').mouseover(function(event){
+	$('.privacy').css('color', '#F9C2C2')
+});
+$('.privacy').click(function(event){
+	$('.privacy').css('color', '#440707')
+});
+$('.privacy').mouseleave(function(event){
+	$('.privacy').css('color', '#FFFFFF')
+});
